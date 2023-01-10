f3da28937e0c3ff2e740d3a43cfe6decbb9482a4
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2023-01-10 20:16:05 +0100

    Add files via upload

---
+++ pythonProject2/main.py
@@ -0,0 +1,10 @@
+from pdfminer.high_level import extract_text
+
+
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
+
+
+
+if __name__ == '__main__':
+    print(extract_text_from_pdf('./resume.pdf')) # noqa: T001
