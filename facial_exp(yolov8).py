1603cb6680a4d4c779bbe8b0a8ebbd132408699a
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-01-23 16:13:36 +0000

    solved device blocking

--- facial_exp(yolov8).py
+++ facial_exp(yolov8).py
@@ -1,27 +1,26 @@
-from roboflow import Roboflow
 import ultralytics
 from ultralytics import YOLO
 import os
 from IPython.display import clear_output, Image
-from torch.cuda import empty_cache
-# ipc_collect -> Force collects GPU memory after it has been released by CUDA IPC.
-# set_sync_debug_mode -> Sets the debug mode for cuda synchronizing operations.
-# communication collectives
 
 from torch import cuda
+from torch.cuda import empty_cache
+import torchvision
+
 clear_output()
 empty_cache()
 cuda.synchronize()
 ultralytics.checks()
 import torch
 
-# if torch.cuda.is_available():
-#     for i in range(torch.cuda.device_count()):
-#         set_per_process_memory_growth(i, True)
 
-        
-model = YOLO("yolov8s.pt")
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
-!yolo task=detect mode=train model=yolov8s.pt data=data.yaml epochs=100 imgsz=640
+device = "cuda" if torch.cuda.is_available() else 'cpu'
+print({device})
+# torch.load("yolov8s.pt")
+model = YOLO("yolov8s.yaml").to(device)
+
+# !TORCH_CUDA_ARCH_LIST="3.5+PTX" TORCH_NVCC_FLAGS="-Xfatbin -compress-all" TORCH_USE_CUDA_DSA=1 yolo task=detect mode=train model=yolov8s.yaml data=data.yaml epochs=50 imgsz=60
 
 
