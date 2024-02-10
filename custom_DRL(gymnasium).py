342674935a4aa5fef4c0d84c1620abe9401dd9ac
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-02-10 21:18:17 +0100

    viz unedited

--- custom_DRL(gymnasium).py
+++ custom_DRL(gymnasium).py
@@ -47,3 +47,32 @@ def GW_model(states, actions):
 
 GW_model = GW_model(states, actions)
 GW_model.summary()
+
+
+
+# ------------------------------------------------------viz-----------------------------------------------
+
+from comet_ml import Experiment
+from comet_ml.integration.gymnasium import CometLogger
+import gymnasium as gym
+
+experiment = Experiment(
+    api_key="YOUR_API_KEY",
+    project_name="YOUR_PROJECT_NAME",
+    workspace="YOUR_WORKSPACE",
+)
+
+env = gym.make('Acrobot-v1', render_mode="rgb_array")
+env = gym.wrappers.RecordVideo(env, 'test')
+env = CometLogger(env, experiment)
+
+for x in range(20):
+    observation, info = env.reset()
+    truncated = False
+    terminated = False
+    while not (truncated or terminated):
+        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        env.render()
+
+env.close() #Uploads video folder 'test' to Comet
+
