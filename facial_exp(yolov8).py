d81adb142096f0a99b5f482eaf4f8544588aa9e3
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-03-24 14:30:27 +0100

    bug fixes

--- facial_exp(yolov8).py
+++ facial_exp(yolov8).py
@@ -2,13 +2,14 @@ import ultralytics
 from ultralytics import YOLO
 import os
 from IPython.display import clear_output, Image
+from pathlib import Path
+import subprocess
 
 from torch import cuda
 from torch.cuda import empty_cache
 import torchvision
 
 clear_output()
-empty_cache()
 cuda.synchronize()
 ultralytics.checks()
 import torch
@@ -18,9 +19,10 @@ os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 print({device})
-# torch.load("yolov8s.pt")
 model = YOLO("yolov8s.yaml").to(device)
+results = model.train(data="datasets/data.yaml", epochs=10)
+torch.save(model.state_dict(), 'yolov8s.pt')
+eval = model.val()
+img = Path("datasets/valid/images/0_10725_jpg.rf.86a1472efaea6984d3f4fa02b0bc88ec.jpg")
 
-! yolo task=detect mode=train model=yolov8s.yaml data=data.yaml epochs=50 imgsz=60
-
-
+empty_cache()
